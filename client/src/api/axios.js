708e5ae4dfab03a15963a43fb8327a799fb1c487
Govardhan Prajapati <govardhanprajapat789@gmail.com>
2026-07-02 22:38:55 +0530

    new

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://hospital-appointment-system-m4hq.onrender.com/api",
+  baseURL: "https://hospital-appointment-system-m4hq.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
 
@@ -20,7 +20,7 @@ api.interceptors.response.use(
       window.location.href = "/login";
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 export default api;
